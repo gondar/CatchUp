d131b014a4397e5b8c9a710c727c9fe83b08bf3a
Marcin <marcin.bazydlo@gmail.com>
2013-02-03 00:53:14 +0000

    Added proper set of tests for game factory.

--- spec/GameFactorySpec.js
+++ spec/GameFactorySpec.js
@@ -1,5 +1,32 @@
 describe("Game Factory", function() {
-	describe("When creating new user", function() {
+	describe("When creating a game", function() {
+		var game;
+		beforeEach(function() {
+			var subject = new GameFactory();
+			var gameSpy = jasmine.createSpyObj("GameController",["Initialize"]);
+			spyOn(window,"GameController").andReturn(gameSpy);
+			
+			game = subject.BuildGame();
+		});
+		
+		it("creates game controller with a new view", function() {
+			 expect(GameController.mostRecentCall.args[0] instanceof GameView).toEqual(true);
+		});
+		
+		it("creates game controller with a new board", function() {
+			 expect(GameController.mostRecentCall.args[1] instanceof GameBoard).toEqual(true);
+		});
+		
+		it("creates game controller with a new player", function() {
+			expect(GameController.mostRecentCall.args[2] instanceof GameObject).toEqual(true);
+		});
+		
+		it("Initializes and returns game controller", function() {
+			expect(game.Initialize).toHaveBeenCalled();
+		});
+	});
+	
+	describe("When creating a new user", function() {
 		var player;
 		beforeEach(function() {
 			var subject = new GameFactory();

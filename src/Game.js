0a7efd2065367448b9fc676ddfdc9e061a2cc0d2
Marcin <marcin.bazydlo@gmail.com>
2013-02-19 21:42:46 +0000

    Refactored Gaem

--- src/Game.js
+++ src/Game.js
@@ -9,12 +9,17 @@ function Game(board, factory, limit, width, height){
 }
 
 Game.prototype.RoundFinished = function(){
-	this._board.MoveDownFallingObjects("FallingObject");
+	var fallingObjectString = "FallingObject";
+	this._board.MoveDownFallingObjects(fallingObjectString);
+	this._addFallingObject(fallingObjectString);
+}
+
+Game.prototype._addFallingObject = function(fallingObjectString){
 	if (this._fallingObjectCount == this._limit)
 		return;
 		
 	this._fallingObjectCount++;
-	var name = "FallingObject"+this._fallingObjectIndex++;
+	var name = fallingObjectString+this._fallingObjectIndex++;
 	var fallingObject = this._factory.BuildFallingObject(this._width);
 	this._board.Add(name,fallingObject);
 }

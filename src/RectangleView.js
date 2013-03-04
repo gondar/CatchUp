0107350a8dd70f081ff7f25d8bd669ee52396a0e
Marcin <marcin.bazydlo@gmail.com>
2013-03-04 00:08:08 +0000

    changed colors

--- src/RectangleView.js
+++ src/RectangleView.js
@@ -2,7 +2,7 @@ function RectangleView(){
 	this._fabric = new fabric.Rect({
 		left: 100,
 		top: 100,
-		fill: '#000000',
+		fill: '#2020a0',
 		width: 20,
 		height: 20
 	});
@@ -17,9 +17,9 @@ RectangleView.prototype.SetModel = function(model){
 RectangleView.prototype.Update = function(){
 	this._fabric.set({left: this.model.Position.x, top: this.model.Position.y});
 	this._fabric.set({width: this.model.Dimensions.Width, height: this.model.Dimensions.Height});
-	if (this._color > 0){
+	if (this._color > 1){
 		this._color -=1;
-		this._fabric.set({fill: "#"+this._color+"00c0c"});
+		this._fabric.set({fill: "#"+this._color+"c1c1c"});
 	}
 	if (this.model.Collision) {
 		this._color = 9;
